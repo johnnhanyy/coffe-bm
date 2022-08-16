d42d25fd9c07ec00736d858e04944d34482fa788
John Hany <johnn@hanii@gmail.com>
2022-08-17 01:36:09 +0200

    first commit

---
+++ src/app/crud/crud-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HotDrinksComponent } from './pages/hot-drinks/hot-drinks.component';
+import { MainComponent } from './pages/main/main.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'crud/main', pathMatch: 'full' },
+  { path: 'crud', redirectTo: 'crud/main', pathMatch: 'full' },
+  { path: 'crud/main', component: MainComponent },
+  { path: 'crud/registration', component: MainComponent },
+  { path: 'crud/login', component: MainComponent },
+  { path: 'crud/register', component: MainComponent },
+  {path:'crud/about',component:AboutComponent},
+  {path:'crud/hot-drinks',component:HotDrinksComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CrudRoutingModule {}
---
+++ src/app/crud/crud.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CrudRoutingModule } from './crud-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { MainComponent } from './pages/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
+import { HotDrinksComponent } from './pages/hot-drinks/hot-drinks.component';
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ProductsComponent,
+    FooterComponent,
+    MainComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutComponent,
+    HeadingComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    SubHeaderComponent,
+    HotDrinksComponent
+  ],
+  imports: [
+    CommonModule,
+    CrudRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+})
+export class CrudModule {}
